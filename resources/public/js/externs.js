aaf32ff9bd41f8177f6721e12c6dc8e89e903afd
Domingo Ernesto Savoretti <esavoretti@gmail.com>
2016-02-20 05:10:50 -0300

    corrected externs.js

--- resources/public/js/externs.js
+++ resources/public/js/externs.js
@@ -3,3 +3,7 @@ var $ = function(){};
 $.draggable = function(){};
 $.droppable = function(){};
 $.attr = function() {};
+$.css = function() {};
+$.on = function() {};
+$.off = function() {};
+$.offset = function() {};
